07aa16470332d4c3e8451e18b04f8f408623ec13
cassiehamley1 <63162478+cassiehamley1@users.noreply.github.com>
2020-07-17 16:21:35 +0100

    Upload of Assignment

---
+++ client.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	var newsSource = document.getElementById("release-template").innerHTML,
+		template = Handlebars.compile(newsSource);
+
+	$.getJSON('release.json', function(json) {
+		var data = template(json);
+		$('.news').html(data);
+	});
+
+	Handlebars.registerHelper('formatDate', function(date, format) {
+		return moment(date).format(format);
+	});
+
+	Handlebars.registerHelper('truncate', function(text, num) {
+		return text.substr(0,num) + '...';
+	});
+});
